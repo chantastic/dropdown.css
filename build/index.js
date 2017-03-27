531604a8d0d1d95d6a0e902f00481bbe19fc16f6
Michael Chan <mijoch@gmail.com>
2017-03-27 09:43:59 -0700

    format: prettier, remove logs, and rubuild

--- build/index.js
+++ build/index.js
@@ -1,45 +1,39 @@
-function closeDropdown (node) {
-  node.setAttribute("aria-expanded", "false")
-  node.querySelector("[aria-haspopup]").focus()
+function closeDropdown(node) {
+  node.setAttribute("aria-expanded", "false");
+  node.querySelector("[aria-haspopup]").focus();
 }
 
-function toggleDropdown (node) {
-  if(node.getAttribute("aria-expanded") === "false") {
-    node.setAttribute("aria-expanded", "true")
-    node.querySelector("[aria-label=close]").focus()
+function toggleDropdown(node) {
+  if (node.getAttribute("aria-expanded") === "false") {
+    node.setAttribute("aria-expanded", "true");
+    node.querySelector("[aria-label=close]").focus();
   } else {
-    node.setAttribute("aria-expanded", "false")
+    node.setAttribute("aria-expanded", "false");
   }
 }
 
-function handleDropdownEvents ({ target, type, keyCode }) {
-  const dropdownRootNode = this // jquery element
+function handleDropdownEvents({ target, type, keyCode }) {
+  const dropdownRootNode = this;
 
-  // if(e.type !== 'click') { return }
-
-  if(
+  if (
     keyCode === 27 &&
     type === "keyup" &&
     dropdownRootNode.classList.contains("dropdown")
   ) {
-    console.log("inside esc")
-    closeDropdown(dropdownRootNode)
+    closeDropdown(dropdownRootNode);
   }
 
-  if(type === "click" && target.classList.contains("dropdown")) {
-    console.log("outside click")
-    closeDropdown(dropdownRootNode)
+  if (type === "click" && target.classList.contains("dropdown")) {
+    closeDropdown(dropdownRootNode);
   }
 
-  if(type === "click" && target.getAttribute("aria-haspopup") === "true") {
-    console.log("normal toggle")
-    toggleDropdown(dropdownRootNode)
+  if (type === "click" && target.getAttribute("aria-haspopup") === "true") {
+    toggleDropdown(dropdownRootNode);
   }
 
-  if(type === "click" && target.getAttribute("aria-label") === "close") {
-    console.log("close clicked")
-    closeDropdown(dropdownRootNode)
+  if (type === "click" && target.getAttribute("aria-label") === "close") {
+    closeDropdown(dropdownRootNode);
   }
 }
 
-module.exports = { handleDropdownEvents: handleDropdownEvents }
+module.exports = { handleDropdownEvents: handleDropdownEvents };
